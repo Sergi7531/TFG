cc6cd1add091fdeee61d21b7a73b8530b4319a7f
raul1411 <raulgrandelopez1411@gmail.com>
2022-04-27 17:51:08 +0200

    Login register design

--- app/src/main/java/com/example/moviez/LoginActivity.java
+++ app/src/main/java/com/example/moviez/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.moviez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public TextView registerText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerText = findViewById(R.id.registerText);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
