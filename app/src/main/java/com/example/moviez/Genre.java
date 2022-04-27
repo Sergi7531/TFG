5aadc7439f8d0e9e7901dcf3f8d3c84aec6bead8
raul1411 <raulgrandelopez1411@gmail.com>
2022-04-27 21:00:05 +0200

    Preferences design

---
+++ app/src/main/java/com/example/moviez/Genre.java
@@ -0,0 +1,14 @@
+package com.example.moviez;
+
+public class Genre {
+
+    private String genreName;
+    private String imageUrl;
+
+    public Genre(String genreName, String imageUrl) {
+        this.genreName = genreName;
+        this.imageUrl = imageUrl;
+    }
+
+
+}
