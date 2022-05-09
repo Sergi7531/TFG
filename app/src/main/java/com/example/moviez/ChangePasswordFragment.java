5ed4c9a7ae46520ea66ecd95b5ff505191234b73
raul1411 <raulgrandelopez1411@gmail.com>
2022-05-09 19:49:02 +0200

    EditProfileFragment     -ChangePasswordFragment  -ChangeUsernameFragment
    activity_main.xml margin changed

--- app/src/main/java/com/example/moviez/ChangePasswordFragment.java
+++ app/src/main/java/com/example/moviez/ChangePasswordFragment.java
@@ -2,22 +2,18 @@ package com.example.moviez;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
+ * Use the {@link ChangePasswordFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditProfileFragment extends Fragment {
+public class ChangePasswordFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,12 +24,7 @@ public class EditProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CardView profileName;
-    public CardView passwordName;
-    public CardView theme;
-    public CardView credits;
-
-    public EditProfileFragment() {
+    public ChangePasswordFragment() {
         // Required empty public constructor
     }
 
@@ -43,11 +34,11 @@ public class EditProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfileFragment.
+     * @return A new instance of fragment ChangePasswordFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EditProfileFragment newInstance(String param1, String param2) {
-        EditProfileFragment fragment = new EditProfileFragment();
+    public static ChangePasswordFragment newInstance(String param1, String param2) {
+        ChangePasswordFragment fragment = new ChangePasswordFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,44 +53,12 @@ public class EditProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        hook(view);
-
-        profileName.setOnClickListener(v -> {
-            setFragment(new ChangeUsernameFragment());
-        });
-
-        passwordName.setOnClickListener(v -> {
-            setFragment(new PasswordFragment());
-        });
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
-    }
-
-    public void hook(View view) {
-        profileName = view.findViewById(R.id.changeName);
-        passwordName = view.findViewById(R.id.changePassword);
-        theme = view.findViewById(R.id.changeDarkLightTheme);
-        credits = view.findViewById(R.id.credits);
-    }
-
-    private void setFragment(Fragment fragment) {
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.main_frame, fragment)
-                .commit();
+        return inflater.inflate(R.layout.fragment_change_password, container, false);
     }
 }
